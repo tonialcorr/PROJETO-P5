91c0f9b66478e89ff4b4ac6f5b4d5acd6135e014
tonialcorr <elias.tonial.correia@escola.pr.gov.br>
2024-09-23 15:32:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+
+}
+
+function draw() {
+ noStroke()
+ fill(135, 206, 235);  
+  rect(0, 0, 500, 250)
+fill(255, 223, 0);
+circle(100, 100, 80); 
+fill(0, 105, 148); 
+  rect(0, 250, 500, 250)
+}
